1d2aa7a0c4440d1db1d2ef31e4e2ed93ad1bb903
史 丹丹 <shimakotomakoto@shimatoMacBook-Pro.local>
2014-12-05 16:32:37 +0800

    added some table view test

--- iOS/MultiViewSample/MultiViewSample/YellowViewController.m
+++ iOS/MultiViewSample/MultiViewSample/YellowViewController.m
@@ -9,7 +9,7 @@
 #import "YellowViewController.h"
 
 @interface YellowViewController ()
-@property (copy, nonatomic) NSArray *dwarwes;
+@property (copy, nonatomic) NSArray *dwarves;
 @end
 
 @implementation YellowViewController
@@ -17,29 +17,11 @@
 - (void)viewDidLoad
 {
     [super viewDidLoad];
-    self.dwarwes = @[@"Sleepy", @"Sneezy", @"Bashful", @"Happy"];
-    UITableView *tableView = (id)[self.view viewWithTag:1];
+    self.dwarves = @[@"Sleepy", @"Sneezy", @"Bashful", @"Happy", @"Dopey", @"Thorin", @"Nori", @"Balin", @"Oril"];
+//    UITableView *tableView = (id)[self.view viewWithTag:1];
 //    UIEdgeInsets contentInset = tableView.contentInset;
 //    contentInset.top = 20;
 //    [tableView setContentInset:contentInset];
-}
-
-- (NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section
-{
-    return [self.dwarwes count];
-}
-
-- (UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath
-{
-    static NSString *simpleTableIdentifier = @"SimpleTableIdentifier";
-    UITableViewCell *cell = [tableView dequeueReusableCellWithIdentifier: simpleTableIdentifier];
-    if (cell == nil) {
-        cell = [[UITableViewCell alloc]
-                initWithStyle:UITableViewCellStyleDefault
-                reuseIdentifier:simpleTableIdentifier];
-    }
-    cell.textLabel.text = self.dwarwes[indexPath.row];
-    return cell;
 }
 
 - (IBAction)yellowButtonPressed {
@@ -50,6 +32,46 @@
                          cancelButtonTitle:@"Yep, iii did"
                          otherButtonTitles:nil];
     [alert show];
+}
+
+- (NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section
+{
+    return [self.dwarves count];
+}
+
+- (UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath
+{
+    static NSString *simpleTableIdentifier = @"SimpleTableIdentifier";
+    UITableViewCell *cell = [tableView dequeueReusableCellWithIdentifier: simpleTableIdentifier];
+    if (cell == nil) {
+        cell = [[UITableViewCell alloc]
+                initWithStyle:UITableViewCellStyleSubtitle
+                reuseIdentifier:simpleTableIdentifier];
+    }
+    
+    UIImage *image = [UIImage imageNamed:indexPath.row %2 == 0 ? @"Image" : @"April-17"];
+    
+    cell.imageView.image = image;
+    cell.textLabel.text = self.dwarves[indexPath.row];
+    
+    cell.detailTextLabel.text = [NSString stringWithFormat: @"This is a %@ @line %d", cell.textLabel.text, indexPath.row];
+    
+    return cell;
+}
+
+- (void)tableView:(UITableView *)tableView didSelectRowAtIndexPath:(NSIndexPath *)indexPath
+{
+    NSString *rowValue = self.dwarves[indexPath.row];
+    NSString *message = [[NSString alloc] initWithFormat:
+                         @"You selected %@", rowValue];
+    UIAlertView *alert = [[UIAlertView alloc]
+                          initWithTitle:@"Row Selected!"
+                          message:message
+                          delegate:nil
+                          cancelButtonTitle:@"Yes I Did"
+                          otherButtonTitles:nil];
+    [alert show];
+    [tableView deselectRowAtIndexPath:indexPath animated:YES];
 }
 
 @end
